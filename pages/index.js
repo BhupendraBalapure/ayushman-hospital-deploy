0808eb6d34df0e6a3513300c8f5c35c2e9767e1b
BhupendraBalapure <bhupendrabalapure@gmail.com>
2025-04-16 00:01:20 +0530

    Add basic Next.js page structure

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Ayushman Hospital</title>
+        <meta name="description" content="Ayushman Hospital Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Ayushman Hospital</h1>
+      </main>
+    </div>
+  )
+} 
